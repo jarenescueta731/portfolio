676fcae0d4f59eab7d51a148363e8407d1fa335a
jarenescueta731 <jaren.escueta@gmail.com>
2016-12-08 17:45:53 -0800

    refactored for functional programming

--- scripts/models/project.js
+++ scripts/models/project.js
@@ -1,49 +1,78 @@
-/* Constructor that will take parsed data from devProjects.json and create
-  new Project object instances*/
-function Project (obj) {
-  for (var prop in obj) {
-    this[prop] = obj[prop];
-  }
-};
+(function(module) { // all Project functions wrapped in an IIFE
+  /* Constructor that will take parsed data from devProjects.json and create
+    new Project object instances*/
+  function Project (obj) {
+    Object.keys(obj).map(function(prop) {
+      return this[prop] = obj[prop];
+    }, this);
+  };
 
-// Constructor function property that tracks the new objects and their key:value pairs
-Project.allProjects = [];
+  // Constructor function property that tracks the new objects and their key:value pairs
+  Project.allProjects = [];
 
-// "Fills" a Handlebars.js template for each object in the devProjects array
-Project.prototype.toHtml = function(templateId) {
-  var theTemplate = Handlebars.compile($(templateId).text());
-  // calculates how many days ago the project was last published on GitHub
-  this.pubDays = parseInt((new Date() - new Date(this.datePublished))/60/60/24/1000);
-  this.pubStat = this.datePublished ? 'Published ' + this.pubDays + ' days ago' : '(draft)';
-  // compile the template from index.html
-  return theTemplate(this);
-};
+  // "Fills" a Handlebars.js template for each object in the devProjects array
+  Project.prototype.toHtml = function(templateId) {
+    var theTemplate = Handlebars.compile($(templateId).text());
+    // calculates how many days ago the project was last published on GitHub
+    this.pubDays = parseInt((new Date() - new Date(this.datePublished))/60/60/24/1000);
+    this.pubStat = this.datePublished ? 'Published ' + this.pubDays + ' days ago' : '(draft)';
+    // compile the template from index.html
+    return theTemplate(this);
+  };
 
-/* Sort projects by most recently published and pushes them into
-  the allProjects array */
-Project.loadAll = function(devProjectsInput) {
-  devProjectsInput.sort(function(currentObject, nextObject) {
-    return (new Date(nextObject.datePublished)) - (new Date(currentObject.datePublished));
-  })
-  .forEach(function(projectObj) {
-    Project.allProjects.push(new Project(projectObj));
-  });
-};
-
-/* Retrieve that data from either localStorage or a remote source, process it,
-  then call a function that renders the page */
-Project.retrieveAll = function() {
-  if (localStorage.devProjects) {
-    var parsed = JSON.parse(localStorage.getItem('devProjects'));
-    Project.loadAll(parsed);
-    console.log('data cached in localStorage');
-    projectView.pageRender();
-  } else {
-    $.getJSON('data/devProjects.json', function(data){
-      Project.loadAll(data);
-      localStorage.setItem('devProjects', JSON.stringify(data));
-      console.log('data from downloaded file');
-      projectView.pageRender();
+  /* Sorts projects by most recently published;
+    also refactored the forEach method to use .map() instead*/
+  Project.loadAll = function(devProjectsInput) {
+    Project.allProjects = devProjectsInput.sort(function(currentObject, nextObject) {
+      return (new Date(nextObject.datePublished)) - (new Date(currentObject.datePublished));
+    })
+    .map(function(projectObj) {
+      return new Project(projectObj);
     });
-  }
-};
+  };
+
+  /* Retrieve that data from either localStorage or a remote source, process it,
+    then call a function that renders the page */
+  Project.retrieveAll = function(nextFunction) {
+    if (localStorage.devProjects) {
+      $.ajax({
+        url: 'data/devProjects.json',
+        type: 'HEAD',
+        success: function(data, textStatus, jqXHR){
+          var lastMod = jqXHR.getResponseHeader('Last-Modified');
+          if (!localStorage.last_mod || lastMod !== localStorage.last_mod) {
+            Project.getAll(nextFunction);
+          } else {
+            Project.loadAll(JSON.parse(localStorage.devProjects));
+            nextFunction();
+          }
+        }
+      });
+    } else {
+      Project.getAll(nextFunction);
+    }
+  };
+
+  // Separate getJSON function for added readability
+  Project.getAll = function(nextFunction) {
+    $.getJSON('data/devProjects.json', function(data,textStatus, jqXHR){
+      localStorage.last_mod = jqXHR.getResponseHeader('Last-Modified');
+      Project.loadAll(data);
+      localStorage.devProjects = JSON.stringify(data);
+      nextFunction();
+    });
+  };
+
+  /* Uses map and reduce methods to get the total word count of all
+    project descriptions */
+  Project.wordCount = function() {
+    return Project.allProjects.map(function(article) {
+      return article.caption.split(' ').length;
+    }).reduce(function(a, b) {
+      return a + b;
+    });
+  };
+
+  // IIFE module that allows access to Project methods
+  module.Project = Project;
+}(window));
--- scripts/views/projectView.js
+++ scripts/views/projectView.js
@@ -1,57 +1,68 @@
-var projectView = {};
+(function(module) { //all projectView functions wrapped in an IIFE
+  var projectView = {};
 
-projectView.handleNavbar = function() {
-  $('#myNavbar .tab:first').on('click', function(e){
-    e.preventDefault();
-    $('.tab-content').hide().fadeIn();
-  });
-  $('#myNavbar .tab:gt(0)').on('click', function(e){
-    e.preventDefault();
-    $('.tab-content').hide();
-    $('#' + $(this).data('content')).fadeIn();
-  });
-};
+  projectView.handleNavbar = function() {
+    $('#myNavbar .tab:first').on('click', function(e){
+      e.preventDefault();
+      $('.tab-content').hide().fadeIn();
+    });
+    $('#myNavbar .tab:gt(0)').on('click', function(e){
+      e.preventDefault();
+      $('.tab-content').hide();
+      $('#' + $(this).data('content')).fadeIn();
+    });
+  };
 
-projectView.expandContract = function() {
-  $('.caption p:nth-of-type(2)').hide();
-  $('a.less').hide();
-  $('#projects').on('click', 'a.more', function(e){
-    e.preventDefault();
-    $(this).parent().find('p').show();
-    $(this).hide();
-    $(this).parent().find('a.less').show();
-  });
-  $('#projects').on('click', 'a.less', function(e){
-    e.preventDefault();
-    $(this).parent().find('p:nth-of-type(2)').hide();
-    $(this).hide();
-    $(this).parent().find('a.more').show();
-  });
-};
+  projectView.expandContract = function() {
+    $('.caption p:nth-of-type(2)').hide();
+    $('a.less').hide();
+    $('#projects').on('click', 'a.more', function(e){
+      e.preventDefault();
+      $(this).parent().find('p').show();
+      $(this).hide();
+      $(this).parent().find('a.less').show();
+    });
+    $('#projects').on('click', 'a.less', function(e){
+      e.preventDefault();
+      $(this).parent().find('p:nth-of-type(2)').hide();
+      $(this).hide();
+      $(this).parent().find('a.more').show();
+    });
+  };
 
-projectView.handleFilter = function() {
-  $('#tag-filter').on('change', function(){
-    if ($(this).val()) {
-      $('article').hide();
-      $('article[data-func="' + $(this).val() + '"]').fadeIn();
-    } else {
-      $('article').fadeIn();
-      $('article.template').hide();
-    }
-  });
-};
+  projectView.handleFilter = function() {
+    $('#tag-filter').on('change', function(){
+      if ($(this).val()) {
+        $('article').hide();
+        $('article[data-func="' + $(this).val() + '"]').fadeIn();
+      } else {
+        $('article').fadeIn();
+        $('article.template').hide();
+      }
+    });
+  };
 
-projectView.pageRender = function() {
-  Project.allProjects.forEach(function(projectObj) {
-    $('#projects').append(projectObj.toHtml('#project-template'));
-    if ($('#tag-filter option[value="' + projectObj.func + '"]').length === 0){
-      $('#tag-filter').append(projectObj.toHtml('#tag-template'));
-    }
-  });
-  projectView.handleNavbar();
-  projectView.handleFilter();
-  projectView.expandContract();
-};
+  projectView.footerStats = function() {
+    $('.words').text(Project.wordCount());
+    $('.lastMod').text(localStorage.last_mod);
+  };
 
-// Retrieve the json data
-Project.retrieveAll();
+  projectView.pageRender = function() {
+    Project.allProjects.forEach(function(projectObj) {
+      $('#projects').append(projectObj.toHtml('#project-template'));
+      if ($('#tag-filter option[value="' + projectObj.func + '"]').length === 0){
+        $('#tag-filter').append(projectObj.toHtml('#tag-template'));
+      }
+    });
+    projectView.handleNavbar();
+    projectView.handleFilter();
+    projectView.expandContract();
+    projectView.footerStats();
+  };
+
+  // Retrieve the json data
+  Project.retrieveAll(projectView.pageRender);
+
+  // IIFE module allows access to projectView methods
+  module.projectView = projectView;
+}(window));
